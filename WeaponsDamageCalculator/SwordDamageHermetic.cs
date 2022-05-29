c935c9f5c6a021386f412e21eb43a7bc7c2865ad
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-29 19:33:32 +0200

    Updated project Added WeaponDamage base class

--- WeaponsDamageCalculator/SwordDamageHermetic.cs
+++ WeaponsDamageCalculator/SwordDamageHermetic.cs
@@ -4,53 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using WeaponsDamageCalculator;
 
 namespace SwordDamageHermeticPrototype
 {
-    public class SwordDamageHermetic
+    public class SwordDamageHermetic : WeaponDamage
     {
         private const int BASE_DAMAGE = 3;
         private const int FLAME_DAMAGE = 2;
-
-        private int roll;
-        private bool flaming;
-        private bool magic;
 
         /// <summary>
         /// Konstruktor oblicza obrażenia na podstawie domyślnych wartości właściwości
         /// Magic i Flaming oraz początkowego rzutu 3d6.
         /// </summary>
         /// <param name="startingRoll">Początkowy rzut 3d6</param>
-       public SwordDamageHermetic(int startingRoll)
+       public SwordDamageHermetic(int startingRoll) : base(startingRoll)
         {
             roll = startingRoll;
             CalculateDamage();            
         }
-
-        /// <summary>
-        /// Ustawia lub pobiera wartość rzutu 3d6.
-        /// </summary>
-        public int Roll { get { return roll; } set { roll = value; CalculateDamage(); } }
-
-        /// <summary>
-        /// Zwraca true, jeśli miecz jest płonący, w przeciwnym razie zwraca false.
-        /// </summary>
-        public bool Flaming {get { return flaming; } set { flaming = value; CalculateDamage(); } }
-
-        /// <summary>
-        /// Zwraca true, jeśli miecz jest magiczny, w przeciwnym razie zwraca false.
-        /// </summary>
-        public bool Magic { get { return magic; } set { magic = value; CalculateDamage(); } }
-
-        /// <summary>
-        /// Zawiera obliczone obrażenia.
-        /// </summary>
-        public int Damage { get; private set; }
-      
-        /// <summary>
-        /// Oblicza obrażenia na podstawie aktualnych wartości właściwości.
-        /// </summary>
-        private void CalculateDamage()
+        protected override void CalculateDamage()
         {
             decimal magicMultiplier = 1M;
             if (Magic) magicMultiplier = 1.75M;
---
+++ WeaponsDamageCalculator/WeaponDamage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponsDamageCalculator
+{
+    public class WeaponDamage
+    {
+        public int roll;
+        public bool flaming;
+        public bool magic;
+
+        /// <summary>
+        /// Zawiera obliczone obrażenia.
+        /// </summar
+        public int Damage { get; protected set; }
+
+        /// <summary>
+        /// Ustawia lub pobiera wartość rzutu 3d6.
+        /// </summary>
+        public int Roll { get { return roll; } set { roll = value; CalculateDamage(); } }
+
+        /// <summary>
+        /// Zwraca true, jeśli miecz jest płonący, w przeciwnym razie zwraca false.
+        /// </summary>
+        public bool Flaming { get { return flaming; } set { flaming = value; CalculateDamage(); } }
+
+        /// <summary>
+        /// Zwraca true, jeśli miecz jest magiczny, w przeciwnym razie zwraca false.
+        /// </summary>
+        public bool Magic { get { return magic; } set { magic = value; CalculateDamage(); } }
+
+        /// <summary>
+        /// Oblicza obrażenia na podstawie aktualnych wartości właściwości.
+        /// </summary>
+        protected virtual void CalculateDamage()
+        {
+            // Przesłaniana w podklasach
+        }
+
+        /// <summary>
+        /// Konstruktor oblicza obrażenia na podstawie domyślnych wartości właściwości
+        /// Magic i Flaming oraz początkowego rzutu 3d6.
+        /// </summary>
+        /// <param name="startingRoll">Początkowy rzut 3d6</param>
+        public WeaponDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+    }
+}
